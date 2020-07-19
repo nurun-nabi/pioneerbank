c2ee3bac9013df926c32c53b168fb555525dda64
nurun-nabi <siddiquee1179@gmail.com>
2020-07-19 20:11:22 +0600

    pioneer bank project complete

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 
-
 //login button event create
 const loginbtn = document.getElementById("loginbtn");
 loginbtn.addEventListener("click", function(){
@@ -10,20 +9,34 @@ transaction.style.display = "block";
 });
 
 //deposit button event create
-
-    const depositBtn = document.getElementById("addDeposit");
+const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
+const depositNumber = getInputNumber("depositAmount");
+updateSpanText("currentDeposit", depositNumber)
+updateSpanText("currentBalance",depositNumber)
 
-    const currentDeposit = document.getElementById("currentDeposit").innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-document.getElementById("currentDeposit").innerText=totalDeposit;
-
-    const currentBalance = document.getElementById("currentBalance").innerText;
-    const currentBalanceNumber = parseFloat(currentBalance);
-    const totalBalance = depositNumber + currentBalanceNumber
-document.getElementById("currentBalance").innerText = totalBalance;
 document.getElementById("depositAmount").value = " ";
 });
+
+
+//withdraw button event create
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+     const withdrawNumber = getInputNumber("withdrawAmount");
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1* withdrawNumber);
+    document.getElementById("withdrawAmount").value = "";
+})
+
+function getInputNumber(id){
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber
+    document.getElementById(id).innerText = totalAmount;
+}
